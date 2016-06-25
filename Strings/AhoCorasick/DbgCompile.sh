9ed4743f575bf39b63d50ce0163eff81ab0d1a75
CStanKonrad <CStanKonrad@users.noreply.github.com>
2016-06-25 15:56:18 +0200

    old changes

---
+++ Strings/AhoCorasick/DbgCompile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#g++ 4.8.4 compatible
+g++ $1.cpp -o $1 -g -Wall -Wextra -pedantic -std=c++11 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fstack-protector
